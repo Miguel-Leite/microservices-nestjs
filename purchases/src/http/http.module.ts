eb9a4a69dadba500c964522da828e01cf9be0b8a
Miguel-Leite <miguelleite200leite@email.com>
2022-12-25 19:17:36 +0100

    initialized repository

---
+++ purchases/src/http/http.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TestController } from './test/test.controller';
+
+@Module({
+    imports: [ConfigModule.forRoot()],
+    controllers: [TestController]
+})
+export class HttpModule {}
---
+++ purchases/src/http/test/test.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthorizationGuard } from '../auth/authorization.guard';
+
+@Controller('test')
+export class TestController {
+
+  @Get('/ok')
+  @UseGuards(AuthorizationGuard)
+  hello() {
+    return 'ok'
+  }
+}
